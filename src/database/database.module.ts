288819e6f4d1593bc9801aff9450962e0ccfcdee
Tomas Juarez <mokajua@gmail.com>
2026-01-09 01:11:37 -0300

    agrego migration seeds para desarrollo

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from './data-source';
+import { AppDataSource } from './data-source';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [TypeOrmModule.forRoot(AppDataSource.options)],
   exports: [TypeOrmModule],
 })
 export class DatabaseModule {}
